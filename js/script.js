1c9d2345166fb2b38df932a99a4adc305e122abe
Madoc Wetsi <madocwetsi2@gmail.com>
2026-07-19 21:53:30 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,290 @@
+const dashboard = document.getElementById('dashboard');
+dashboard.addEventListener('click', (e) => {
+  e.preventDefault();
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'block' ;
+
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'none' ;
+
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'none' ;
+
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'none' ;
+
+  const announcements1 = doucument.getElementById('announcements1');
+  announcements1.style.display = 'none' ;
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'none' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'none' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'none' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'none' ;
+
+
+});
+
+const mysubjects = document.getElementById('mysubjects');
+mysubjects.addEventListener('click', (e) => {
+  e.preventDefault();
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'block' ;
+
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'none' ;
+
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'none' ;
+
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'none' ;
+
+  const announcements1 = doucument.getElementById('announcements1');
+  announcements1.style.display = 'none' ;
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'none' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'none' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'none' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'none' ;
+
+});
+
+const assignments = document.getElementById('assignments');
+assignments.addEventListener('click', (e) => {
+  e.preventDefault();
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'block' ;
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'none' ;
+
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'none' ;
+
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'none' ;
+
+  const announcements1 = doucument.getElementById('announcements1');
+  announcements1.style.display = 'none' ;
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'none' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'none' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'none' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'none' ;
+});
+
+const grades = document.getElementById('grades');
+grades.addEventListener('click', (e) => {
+  e.preventDefault();
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'block' ;
+
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'none' ;
+
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'none' ;
+
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'none' ;
+
+  const announcements1 = doucument.getElementById('announcements1');
+  announcements1.style.display = 'none' ;
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'none' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'none' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'none' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'none' ;
+});
+
+const announcements = document.getElementById('announcements');
+announcements.addEventListener('click', (e) => {
+  e.preventDefault();
+  const announcements1 = document.getElementById('announcements1');
+  announcements1.style.display = 'block' ;
+
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'none' ;
+
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'none' ;
+
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'none' ;
+
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'none' ;
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'none' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'none' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'none' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'none' ;
+
+});
+
+const calendar = document.getElementById('calendar');
+calendar.addEventListener('click', (e) => {
+  e.preventDefault();
+  const announcements1 = document.getElementById('announcements1');
+  announcements1.style.display = 'none' ;
+
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'none' ;
+
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'none' ;
+
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'none' ;
+
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'none' ;
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'none' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'none' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'none' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'block' ;
+
+});
+
+const messages = document.getElementById('messages');
+messages.addEventListener('click', (e) => {
+  e.preventDefault();
+  const announcements1 = document.getElementById('announcements1');
+  announcements1.style.display = 'none' ;
+
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'none' ;
+
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'none' ;
+
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'none' ;
+
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'none' ;
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'block' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'none' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'none' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'none' ;
+
+});
+
+const profile = document.getElementById('profile');
+profile.addEventListener('click', (e) => {
+  e.preventDefault();
+  const announcements1 = document.getElementById('announcements1');
+  announcements1.style.display = 'none' ;
+
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'none' ;
+
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'none' ;
+
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'none' ;
+
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'none' ;
+
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'none' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'block' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'none' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'none' ;
+
+});
+
+const settings = document.getElementById('settings');
+settings.addEventListener('click', (e) => {
+  e.preventDefault();
+  const announcements1 = document.getElementById('announcements1');
+  announcements1.style.display = 'none' ;
+
+  const grades1 = document.getElementById('grades1');
+  grades1.style.display = 'none' ;
+
+  const dashboard1 = document.getElementById('dashboard1');
+  dashboard1.style.display = 'none' ;
+
+  const mysubjects1 = document.getElementById('mysubjects1');
+  mysubjects1.style.display = 'none' ;
+
+  const assignments1 = document.getElementById('assignments1')
+  assignments1.style.display = 'none' ;
+
+  const messages1 = document.getElementById('messages1');
+  messages1.style.display = 'none' ;
+
+  const profile1 = document.getElementById('profile1');
+  profile1.style.display = 'none' ;
+
+ const settings1 = document.getElementById('settings1');
+  settings1.style.display = 'block' ;
+
+  const calendar1 = document.getElementById('calendar1');
+  calendar1.style.display = 'none' ;
+
+});
+
+
+
+
